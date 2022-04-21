718d0f8189a2261e2091e13f6b74f9732867a18a
CLg003 <clairemjlaing@gmail.com>
2022-04-21 09:45:32 +0100

    Hidden file variable test.

--- client/src/SightingsGrid.js
+++ client/src/SightingsGrid.js
@@ -1,4 +1,7 @@
 import SightingCard from "./SightingCard";
+import { apiKey } from "./file_to_ignore";
+
+
 const SightingsGrid = ({sightings, removeSighting}) => {
     const sightingsList = sightings.map((sighting) =>{
         return <SightingCard sighting={sighting} key={sighting._id} removeSighting={removeSighting} />
@@ -6,6 +9,9 @@ const SightingsGrid = ({sightings, removeSighting}) => {
     
     return (
         <>
+            <p>The first key is: {apiKey.firstKey}</p>
+            <p>The second key is: {apiKey.secondKey}</p>
+            <p>The third key is: {apiKey.thirdKey}</p>
             {sightingsList}
         </>
     );
---
+++ client/src/file_to_ignore.js
@@ -0,0 +1,5 @@
+export const apiKey = {
+    firstKey : 'key1_123456',
+    secondKey : 'key2_56789',
+    thirdKey : 'key3_101010'
+  };
